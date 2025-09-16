2494e2b7098bbcde102b675c8d1f29fa3f075b94
Strakhov Nicholas <strahovnikolaj27@gmail.com>
2025-09-16 23:21:31 +0300

    Теперь контроллеры соответствуют REST

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,50 +1,59 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
-import ru.yandex.practicum.filmorate.validator.UserValidator;
+import ru.yandex.practicum.filmorate.service.UserService;
 
 import java.util.*;
 
 @RestController
 @RequestMapping("/users")
+@RequiredArgsConstructor(onConstructor_ = {@Autowired})
 @Slf4j
 public class UserController {
-
-    private final Map<Long, User> users = new HashMap<>();
-    private long nextId = 1;
+    private final UserService userService;
 
     @PostMapping
     public User addUser(@RequestBody User user) {
-        UserValidator.validate(user);
+        return this.userService.addUser(user);
+    }
 
-        user.setId(nextId++);
-        users.put(user.getId(), user);
+    @GetMapping("/{id}")
+    public User getUser(@PathVariable String id) {
+        Optional<User> user = this.userService.getUser(Long.parseLong(id));
+        return user.orElse(null);
+    }
 
-        log.debug("У добавляемого пользователя id: {}", user.getId());
-        log.info("Пользователь '{}' успешно создан с id={}", user.getLogin(), user.getId());
+    @PutMapping("/{id}/friends/{friendId}")
+    public void addFriend(@PathVariable String id, @PathVariable String friendId) {
+        this.userService.addFriend(Long.parseLong(id), Long.parseLong(friendId));
+    }
 
-        return user;
+    @DeleteMapping("/{id}/friends/{friendId} ")
+    public void deleteFriend(@PathVariable String id, @PathVariable String friendId) {
+        this.userService.removeFriend(Long.parseLong(id), Long.parseLong(friendId));
+    }
+
+    @GetMapping("/{id}/friends")
+    public Set<User> getFriends(@PathVariable String id) {
+        return this.userService.getFriends(Long.parseLong(id));
+    }
+
+    @GetMapping("/{id}/friends/common/{otherId}")
+    public Set<User> getCommonFriends(@PathVariable String id, @PathVariable String otherId) {
+        return this.userService.getCommonFriends(Long.parseLong(id), Long.parseLong(otherId));
     }
 
     @GetMapping
-    public List<User> getAllUsers() {
-        return new ArrayList<>(users.values());
+    public Set<User> getAllUsers() {
+        return this.userService.getUsers();
     }
 
     @PutMapping
     public User updateUser(@RequestBody User user) {
-        UserValidator.validate(user);
-
-        if (!users.containsKey(user.getId())) {
-            throw new ValidationException("Пользователь с таким id не найден");
-        }
-
-        users.put(user.getId(), user);
-        log.info("Пользователь '{}' успешно обновлён", user.getLogin());
-
-        return user;
+        return this.userService.updateUser(user);
     }
 }
